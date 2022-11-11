d44f7463aa6956d2c7071bfcd9d5782e085c0aee
NippunSharma <inbox.nippun@gmail.com>
2022-11-11 19:42:49 +0530

    first commit

---
+++ includes/armadillo-11.4.2/tests1/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nippun/wasm/webml/armadillo-11.4.2/tests1
+# Build directory: /home/nippun/wasm/webml/armadillo-11.4.2/tests1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(smoke_test "/home/nippun/wasm/webml/armadillo-11.4.2/tests1/smoke_test")
+set_tests_properties(smoke_test PROPERTIES  _BACKTRACE_TRIPLES "/home/nippun/wasm/webml/armadillo-11.4.2/tests1/CMakeLists.txt;5;add_test;/home/nippun/wasm/webml/armadillo-11.4.2/tests1/CMakeLists.txt;0;")
